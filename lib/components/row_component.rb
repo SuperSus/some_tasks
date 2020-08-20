426f201d998b413feb8596fd8c240104d4d1279c
SuperSus <nickvasilevski35@gmail.com>
2020-08-21 02:03:13 +0300

    move cell_component creation into row_component

--- lib/components/row_component.rb
+++ lib/components/row_component.rb
@@ -1,16 +1,35 @@
 # frozen_string_literal: true
 
+require_relative('cell_component')
+
 class RowComponent
   attr_reader :cells, :height
 
-  def initialize(cells, height:)
-    @cells = cells
+  def initialize(row, cell_widths:, height:)
     @height = height
+    @cells = build_cells(row, cell_widths)
   end
 
   def render
     height.times.map do |index|
       cells.map { |cell| cell.render(index) }.join
     end.join("\n")
+  end
+
+  private
+
+  def build_cells(row, cell_widths)
+    row.map.with_index do |cell, column_index|
+      position = :inner
+      position = :left if column_index.zero?
+      position = :right if column_index == cell_widths.size
+
+      CellComponent.new(
+        cell,
+        position: position,
+        height: height,
+        width: cell_widths[column_index]
+      )
+    end
   end
 end
--- lib/components/table_component.rb
+++ lib/components/table_component.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative('cell_component')
 require_relative('row_component')
 
 class TableComponent
@@ -27,22 +26,18 @@ class TableComponent
   end
 
   def border
-    column_widths.map { |width| '-' * (width - 1) + '+' }.join
+    border = column_widths.map { |width| '-' * (width - 1) + '+' }.join
+    "+#{border[1..-1]}"
   end
 
   def rows
     @rows ||= begin
       table.rows.map.with_index do |row, row_index|
-        row_cells = row.map.with_index do |cell, column_index|
-          CellComponent.new(
-            cell,
-            position: :inner,
-            height: row_heights[row_index],
-            width: column_widths[column_index]
-          )
-        end
-
-        RowComponent.new(row_cells, height: row_heights[row_index])
+        RowComponent.new(
+          row,
+          cell_widths: column_widths,
+          height: row_heights[row_index]
+        )
       end
     end
   end
@@ -52,7 +47,7 @@ class TableComponent
   end
 
   def column_widths
-    @column_widths ||= table.column_widths.map { |width| width + padding + 1 }
+    @column_widths ||= table.column_widths.map { |width| width + padding + 2 }
   end
 
   def row_heights
